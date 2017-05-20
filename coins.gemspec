659f40e858bed45b4035f295df23e6f088dae6bd
Demonstrandum <knutsen@jetspace.co>
2017-05-20 15:01:07 +0100

    Added help message

--- coins.gemspec
+++ coins.gemspec
@@ -1,9 +1,9 @@
 Gem::Specification.new do |s|
   s.name        = 'coins'
-  s.version     = '0.1.0'
+  s.version     = '0.1.1'
   s.required_ruby_version = '>= 1.9.2'
   s.executables << 'coins'
-  s.date        = '2017-05-16'
+  s.date        = '2017-05-20'
   s.summary     = "Terminal currency converter"
   s.description = "A live terminal currency exchange rate converter"
   s.authors     = ["Demonstrandum"]
--- lib/coins.rb
+++ lib/coins.rb
@@ -15,6 +15,23 @@ class Coins
     end
   end
 
+  def help
+    puts "\n   A simple live terminal currency exchange rate converter in Ruby\n"\
+         "   The currencies are retrieved from fixer.io which is a\n"\
+         "   'Foreign exchange rates and currency conversion API'\n"\
+         "   which is a JSON API published by the 'European Central Bank'.\n\n"\
+         "   The rates are updated daily around 4 PM CET\n\n"\
+         "   Usage:\n"\
+         "   \"coins [rate(s)] [base/against] / [from] num(float/int ){CUR} [to] {CUR}\"\n"\
+         "   where CUR is the three letter currency "
+         "   (e.g. USD, GBP NOK, EUR, RUB, etc...)\n\n"\
+         "   use 'from `n` `CUR` to `CUR(other)`' toconvert between currencies.\n\n"\
+         "   use 'rates' to list all rates (see `base` also) or 'rate `CUR`'\n"\
+         "   to see the rate for one single currency.\n\n"\
+         "   use 'base `CUR`' or 'agains `CUR`' to change what the exchange rates compare to.\n"
+         "   Default rate base is USD\n\n"
+  end
+
   def args? this
     if ARGV.include? this
       return true
@@ -38,6 +55,11 @@ class Coins
 
 
   def parseArgs
+    if ARGV.empty? || args?('help')
+      help
+      exit 1
+    end
+
     if args?('base')
       setRate(ARGV[ARGV.index('base') + 1].upcase)
     end
